bc34dd956a8c65af006095b50e4caaf65aa1d65b
Nelson Gimon <nelson.gimon@gmail.com>
2023-07-11 10:19:35 -0400

    Auth component updated

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -99,7 +99,7 @@ const Auth = () => {
                                 </span>
                             )}
                         </button>
-                        <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                        {/* <div className="flex flex-row items-center gap-4 mt-8 justify-center">
                             <div
                                 onClick={() => signIn('google', { callbackUrl: '/profiles' })}
                                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
@@ -110,7 +110,7 @@ const Auth = () => {
                                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
                                 <FaGithub size={30} />
                             </div>
-                        </div>
+                        </div> */}
                         <p className="text-neutral-500 mt-12">
                             {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
                             <span
